6aceae7a919e89e13534f9e96e7b8a03511212c1
DAVI <daviaragaoyt@gmail.com>
2024-09-05 18:43:26 -0300

    87% feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Cadastro from './Cadastro/index'; // Importar o componente de Cadastro
+import CadastroPaciente from './Cadastro/index'; // Importar o componente de Cadastro
 import Home from './Home';
-import Dentes from './Dentes';
+import DenteForm from './Dentes';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes >
         <Route  path="/" element={<Home />} />
-        <Route path="/Dentes" element={<Dentes />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/Dentes" element={<DenteForm />} />
+        <Route path="/cadastro" element={<CadastroPaciente />} />
       </Routes>
     </BrowserRouter>
   );
