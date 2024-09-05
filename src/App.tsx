1541fe9eef97160616051aef44b4b8becedf966a
DAVI <daviaragaoyt@gmail.com>
2024-09-05 19:00:06 -0300

    Testezin

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cadastro from './cadastro'; // Importar o componente de Cadastro
+import Home from './home';
+import Dentes from './dentes';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes >
+        <Route  path="/" element={<Home />} />
+        <Route path="/dentes" element={<Dentes />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
