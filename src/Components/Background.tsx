eae91492bfa2f34df8def549064a4a6def98a383
Davi Aragao <daviaragaodf1@gmail.com>
2024-10-02 21:19:16 -0300

    results finalizados

--- src/Components/Background.tsx
+++ src/Components/Background.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Backgroud: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="relative min-h-screen bg-cover bg-center flex items-center justify-center"
-      style={{ backgroundImage: `url(../../public/images/Background.png)`, backgroundSize: 'contain' }}
+      style={{ backgroundImage: `url(/images/Background.png)`, backgroundSize: 'contain' }}
     >
       {/* Camada escura sobre a imagem */}
       <div className="absolute inset-0  bg-sky-600 opacity-50"></div>
